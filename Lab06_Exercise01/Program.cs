e9444edd94979d5a70246d54786e9e1f0c6e7d7b
Salinatooninter <66030284@kmitl.ac.th>
2025-01-10 16:52:41 +0700

    Add files via upload

---
+++ Lab06_Exercise01/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+   System.Console.WriteLine("Hello, I'm 66030284");
+   System.Console.WriteLine("Please enter your name");
+   var name = System.Console.ReadLine();  
+   System.Console.WriteLine($"Your name is {name}");
+   
